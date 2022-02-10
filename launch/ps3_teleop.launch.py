d421747e993ea64bcf1a0d599680ad52859edfa7
hseachrist <hseachrist@gmail.com>
2022-02-10 16:59:35 -0500

    Port ps3_teleop

--- launch/ps3_teleop.launch.py
+++ launch/ps3_teleop.launch.py
@@ -2,25 +2,32 @@ import launch
 import launch.actions
 from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 import os
 
 def generate_launch_description():
     # Read in the vehicle's namespace through the command line or use the default value one is not provided
-    launch.actions.DeclareLaunchArgument(
-        "robot", 
-        default_value="tempest",
-        description="Name of the vehicle",
-    )
 
-    robot = 'tempest'
     # declare the path to the robot's vehicle description file
-    config = os.path.join(
+    config = PathJoinSubstitution([
         get_package_share_directory('riptide_descriptions2'),
         'config',
-        robot + '.yaml'
-    )
+        LaunchConfiguration("robot_yaml")
+    ])
 
     return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+            "robot", 
+            default_value="tempest",
+            description="Name of the vehicle",
+        ),
+
+        launch.actions.DeclareLaunchArgument(
+            "robot_yaml", 
+            default_value=[LaunchConfiguration("robot"), '.yaml'],
+            description="Name of the vehicle",
+        ),
+
         # create the nodes    
         launch_ros.actions.Node(
             package='riptide_teleop2',
